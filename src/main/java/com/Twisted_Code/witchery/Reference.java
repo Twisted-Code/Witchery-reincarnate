a7a81999429d65eba018569a3d124287aedae1dd
Phillip MacKelvey <mackslikesminecraft@gmail.com>
2018-03-28 20:14:36 -0400

    added event handler stubs for mod initialization, added an AcceptedMinecraftVersion range, and implemented the attributes from Reference.class

--- src/main/java/com/Twisted_Code/witchery/Reference.java
+++ src/main/java/com/Twisted_Code/witchery/Reference.java
@@ -4,5 +4,6 @@ public class Reference {
 	public static final String MODID = "witcheryreincarnate";
 	public static final String NAME = "Witchery: Reincarnate";
 	public static final String VERSION = "0.1.0";
+	public static final String ACCEPTEDMCVERSIONS = "[1.10.2,1.11.2]";
 
 }
--- src/main/java/com/Twisted_Code/witchery/WitcheryReincarnate.java
+++ src/main/java/com/Twisted_Code/witchery/WitcheryReincarnate.java
@@ -1,5 +1,25 @@
 package com.Twisted_Code.witchery;
 
-public class WitcheryReincarnate {
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
+@Mod(modid = Reference.MODID, name = Reference.NAME, version = Reference.VERSION, acceptedMinecraftVersions = Reference.ACCEPTEDMCVERSIONS)
+public class WitcheryReincarnate {
+	
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event) {
+		
+	}
+	@EventHandler
+	public void init(FMLInitializationEvent event) {
+		
+	}
+	@EventHandler
+	public void postInit(FMLPostInitializationEvent event) {
+		
+	}
+	
 }
