d4cb185753e18a51b26e68bc45a19828f37fc1cf
Phillip MacKelvey <mackslikesminecraft@gmail.com>
2018-04-21 14:34:45 -0400

    Successfully textured the new block. it's the same texture on all six sides (no "log core"), but I'll fix that in the next commit

--- src/main/java/com/Twisted_Code/witchery/blocks/BlockRowanLog.java
+++ src/main/java/com/Twisted_Code/witchery/blocks/BlockRowanLog.java
@@ -3,6 +3,7 @@ package com.Twisted_Code.witchery.blocks;
 import com.Twisted_Code.witchery.Reference;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.util.ResourceLocation;
 
@@ -12,6 +13,9 @@ public class BlockRowanLog extends Block{
 		super(Material.WOOD);
 		this.setUnlocalizedName(unlocalizedName);
 		this.setRegistryName(new ResourceLocation(Reference.MODID, registryName));
+		this.setHardness(1);
+		this.setResistance(5);
+		this.setSoundType(SoundType.WOOD);
 	}
 	
 }
